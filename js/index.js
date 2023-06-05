a5da07490cfd71ed3d7f785a92e4b1e4f256444a
Emmanuel <emmanuel_rueda@live.com.mx>
2023-06-04 23:03:13 -0600

    actualizacion de la estructura del html y css

--- js/index.js
+++ js/index.js
@@ -1,55 +1,55 @@
 //JS para esconder el mesaje que aparecera despues de encriptar un texto
-window.onload = esconderMensaje;
-function esconderMensaje() {
-  document.getElementById("funciones-mensaje").style.display = "none";
+window.onload = hiddeMessage;
+function hiddeMessage() {
+  document.getElementById("output-msg").style.display = "none";
 }
 
 //JS para validar sin el texto tiene minusculas, acentos o mayusculas
 function validar() {
-  var texto = document.getElementById("funciones-entrada").value;
+  var texto = document.getElementById("input-text").value;
   var patron = /[áéíóúÁÉÍÓÚÜüÑñA-Z0-9]/;
 
   if (
     texto.match(patron) ||
-    document.getElementById("funciones-entrada").value == ""
+    document.getElementById("input-text").value == ""
   ) {
     alerta();
-    document.getElementById("funciones-entrada").value = "";
+    document.getElementById("input-text").value = "";
     return false;
   }
-  document.getElementById("funciones-icono").style.color = "#474747";
-  document.getElementById("funciones-letra").style.color = "#474747";
-  document.getElementById("funciones-entrada").value = "";
+  document.getElementById("alert-icon").style.color = "#474747";
+  document.getElementById("alert-text").style.color = "#474747";
+  document.getElementById("input-text").value = "";
 }
 
 function alerta() {
-  document.getElementById("funciones-icono").style.color = "red";
-  document.getElementById("funciones-letra").style.color = "red";
-  document.getElementById("funciones-imagen").style.display = "block";
-  document.getElementById("funciones-mensaje").style.display = "none";
+  document.getElementById("alert-icon").style.color = "red";
+  document.getElementById("alert-text").style.color = "red";
+  document.getElementById("output-img").style.display = "block";
+  document.getElementById("output-msg").style.display = "none";
 }
 
 function mostrar() {
-  document.getElementById("funciones-imagen").style.display = "none";
-  document.getElementById("funciones-mensaje").style.display = "block";
+  document.getElementById("output-img").style.display = "none";
+  document.getElementById("output-msg").style.display = "block";
 }
 
 //JS para encriptar un mensaje
 function encriptar() {
-  var texto = document.getElementById("funciones-entrada").value;
+  var texto = document.getElementById("input-text").value;
   var txtEncriptado = texto
     .replace(/e/gi, "enter")
     .replace(/i/gi, "imes")
     .replace(/a/gi, "ai")
     .replace(/o/gi, "ober")
     .replace(/u/gi, "ufat");
-  document.getElementById("salida").value = txtEncriptado;
+  document.getElementById("output-text").value = txtEncriptado;
 }
 
 //JS para desencriptar el mensaje
 
 function desencriptar() {
-  var texto = document.getElementById("funciones-entrada").value;
+  var texto = document.getElementById("input-text").value;
   var txtDesencriptado = texto
     .replace(/enter/gi, "e")
     .replace(/imes/gi, "i")
@@ -57,14 +57,14 @@ function desencriptar() {
     .replace(/ober/gi, "o")
     .replace(/ufat/gi, "u");
 
-  document.getElementById("salida").value = txtDesencriptado;
+  document.getElementById("output-text").value = txtDesencriptado;
 }
 
 //JS pra copiar el texto
 function copiar() {
-  var texto = document.getElementById("salida");
+  var texto = document.getElementById("output-text");
   texto.select();
   document.execCommand("copy");
-  document.getElementById("funciones-imagen").style.display = "block";
-  document.getElementById("funciones-mensaje").style.display = "none";
+  document.getElementById("output-img").style.display = "block";
+  document.getElementById("output-msg").style.display = "none";
 }
